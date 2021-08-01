01f79c7947440f7dada00fecc9688a84bdc5a0c2
Rebecca Burch <58955802+thinklikeadesigner@users.noreply.github.com>
2021-08-02 01:58:39 +0200

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,10 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "rebecca-portfolio",
+    title: "My First Gatsby Site",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };
